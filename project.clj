7449c802445c3fcefb73b8cca9cf7770193b7cb4
Silas Davis <silas@erisindustries.com>
2017-05-03 15:28:37 +0100

    add lein binplus and docstring

--- project.clj
+++ project.clj
@@ -6,5 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [io.forward/yaml "1.0.6"]]
   :main ^:skip-aot lockfiles.core
+  :bin {:name "lockmerge"
+        :bin-path "~/.local/bin"}
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/lockfiles/core.clj
+++ src/lockfiles/core.clj
@@ -4,11 +4,22 @@
             [yaml.reader :as yamlreader]))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Merges Trash vendor.conf lockfiles together. Takes a list of paths to vendor.conf files as its arguments.
+  Dependencies appearing in lockfiles appearing later in the argument list take precedence over those appearing earlier.
+  Therefore provide lockfiles intended to override later than those containing the base dependencies."
   [& args]
-  (let [flattened (for [imports (map (comp :import yaml/parse-string) (map slurp args))
-                        kvs (map (juxt :package identity) imports)] kvs)
-        merged (into {} flattened)
-        lockfile {:import (sort-by :package (vals merged))}
-        yaml-string (yaml/generate-string lockfile :dumper-options {:flow-style :block})]
-    (println yaml-string)))
+  (if (> (count args) 0)
+    (let [flattened (for [imports (map (comp :import yaml/parse-string) (map slurp args))
+                          kvs (map (juxt :package identity) imports)] kvs)
+          merged (into {} flattened)
+          lockfile {:import (sort-by :package (vals merged))}
+          yaml-string (yaml/generate-string lockfile :dumper-options {:flow-style :block})]
+      (println yaml-string))
+    (binding [*out* *err*]
+      (println "Merge together Trash lockfiles.
+
+Usage:
+      lockmerge <paths>...
+
+Where paths is list of paths to vendor.conf lockfiles to be merged together.
+Those provided later override those provided later."))))
