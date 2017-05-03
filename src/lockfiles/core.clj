da142585d264b945148bc05a7054e77bc6ae664b
Silas Davis <silas@erisindustries.com>
2017-05-03 14:25:22 +0100

    yaml merging

--- src/lockfiles/core.clj
+++ src/lockfiles/core.clj
@@ -1,11 +1,14 @@
 (ns lockfiles.core
   (:gen-class)
   (:require [yaml.core :as yaml]
-            [yaml.reader :as yamlreader]
-            [flatland.useful.map :as flatmap]))
+            [yaml.reader :as yamlreader]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (for [ys (map yaml/parse-string (map slurp args))]
-    (println ys)))
+  (let [flattened (for [imports (map (comp :import yaml/parse-string) (map slurp args))
+                        kvs (map (juxt :package identity) imports)] kvs)
+        merged (into {} flattened)
+        lockfile {:import (sort-by :package (vals merged))}
+        yaml-string (yaml/generate-string lockfile :dumper-options {:flow-style :block})]
+    (println yaml-string)))
